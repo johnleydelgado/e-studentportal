54204daf79a707bffc21b91e23f67b882f1aff0c
ley <johnley00@gmail.com>
2017-03-08 00:22:54 +0800

    update

--- index.php
+++ index.php
@@ -171,18 +171,51 @@ footer a, footer a:link {
 
     </style>
 	</head>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js">
-
-  window.fbAsyncInit = function() {
+<body>
+  <script>
+window.fbAsyncInit = function() {
     FB.init({
       appId      : '1647204945583377',
       xfbml      : true,
       version    : 'v2.8'
     });
 
-    // ADD ADDITIONAL FACEBOOK CODE HERE
-  
+    // Place following code after FB.init call.
+
+    function onLogin(response) {
+      if (response.status == 'connected') {
+        FB.api('/me?fields=first_name', function(data) {
+          var welcomeBlock = document.getElementById('fb-welcome');
+          welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+        });
+      }
+    }
+
+    FB.getLoginStatus(function(response) {
+      // Check login status on load, and if the user is
+      // already logged in, go directly to the welcome message.
+      if (response.status == 'connected') {
+        onLogin(response);
+      } else {
+        // Otherwise, show Login dialog first.
+        FB.login(function(response) {
+          onLogin(response);
+        }, {scope: 'user_friends, email'});
+      }
+    });
+    // end Place following code after FB.init call.
+
+    // Add a dialog to add this app as a pagetab to any fb page you have administrative access to 
+    // (and remove it once its added if you don't want it to be added to any further pages)
+    // FB.ui({
+    //   method: 'pagetab',
+    //   redirect_uri: 'https://www.dgm.org/donations'
+    // }, function(response){});
+
+  }; 
+
+
+
 
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
@@ -191,34 +224,11 @@ footer a, footer a:link {
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-
-  function onLogin(response) {
-  if (response.status == 'connected') {
-    FB.api('/me?fields=first_name', function(data) {
-      var welcomeBlock = document.getElementById('fb-welcome');
-      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-    });
-  }
-}
-
-FB.getLoginStatus(function(response) {
-  // Check login status on load, and if the user is
-  // already logged in, go directly to the welcome message.
-  if (response.status == 'connected') {
-    onLogin(response);
-  } else {
-    // Otherwise, show Login dialog first.
-    FB.login(function(response) {
-      onLogin(response);
-    }, {scope: 'user_friends, email'});
-  }
-});
-  };
   </script>
 
 
 
-<body>
+
 
   <div class="wrapper">
   <form class="login">

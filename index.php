1531afd6794ff015791d875b3450cafbed15a047
ley <johnley00@gmail.com>
2017-03-07 22:31:38 +0800

    update

--- index.php
+++ index.php
@@ -173,34 +173,7 @@ footer a, footer a:link {
 	</head>
 <?php
   <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js">
-   FB.init({
-    appId      : '1647204945583377',
-    status     : true,
-    xfbml      : true,
-    version    : 'v2.8' // or v2.6, v2.5, v2.4, v2.3
-  });
-  
-  function onLogin(response) {
-  if (response.status == 'connected') {
-    FB.api('/me?fields=first_name', function(data) {
-      var welcomeBlock = document.getElementById('fb-welcome');
-      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-    });
-  }
-}
 
-FB.getLoginStatus(function(response) {
-  // Check login status on load, and if the user is
-  // already logged in, go directly to the welcome message.
-  if (response.status == 'connected') {
-    onLogin(response);
-  } else {
-    // Otherwise, show Login dialog first.
-    FB.login(function(response) {
-      onLogin(response);
-    }, {scope: 'user_friends, email'});
-  }
-});
   
   </script>
 ?>
